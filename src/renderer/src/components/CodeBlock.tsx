28218040df4337ff8d6d2f7e7f721fce5c30a31b
Jonah <jonahega@gmail.com>
2024-09-12 02:26:49 +0900

    non memo

--- src/renderer/src/components/CodeBlock.tsx
+++ src/renderer/src/components/CodeBlock.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, memo } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import { runPythonCode } from '@renderer/lib/ipcFunctions'
 import { Play, Download } from 'lucide-react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
@@ -21,114 +21,113 @@ interface CodeBlockProps {
   index: number
 }
 
-const CodeBlock: React.FC<CodeBlockProps> = memo(
-  ({ conversation, messageId, code, handleExecutionResult, language, inline, index }) => {
-    const [result, setResult] = useState<ExecutionResult | null>(null)
-    const [base64Figures, setBase64Figures] = useState<string[]>([])
-
-    useEffect(() => {
-      const existingResult = conversation?.messages.find(
-        (message) => message.id === messageId && message.executionResults
-      )?.executionResults?.[0]
-
-      if (existingResult) {
-        setResult(existingResult)
-
-        if (existingResult.figurePaths) {
-          Promise.all(existingResult.figurePaths.map(loadBase64Data)).then((base64Images) => {
-            const filteredImages = base64Images.filter((base64) => base64 !== null) as string[]
-            setBase64Figures(filteredImages)
-          })
-        }
-      }
-    }, [conversation, messageId])
-
-    const handleRun = async () => {
-      try {
-        const figureDir = await getConversationImagesDir(conversation.id)
-        const result = await runPythonCode(figureDir, code)
-        await saveConversationWithPythonResult(conversation, messageId, result)
-        setResult(result)
-
-        if (result.figurePaths) {
-          const base64Images = (await Promise.all(result.figurePaths.map(loadBase64Data))).filter(
-            (base64) => base64 !== null
-          ) as string[]
-          setBase64Figures(base64Images)
-        }
-        handleExecutionResult(messageId, result)
-      } catch (error) {
-        const result: ExecutionResult = {
-          code,
-          error: (error as Error).message
-        }
-        setResult(result)
-        handleExecutionResult(messageId, result)
-      }
-    }
-
-    if (inline) {
-      return (
-        <code key={index} className="bg-gray-300 rounded px-1 py-0.5 text-sm">
-          {code}
-        </code>
-      )
-    }
-
-    return (
-      <div>
-        {language === 'python' && (
-          <div className="flex justify-between items-center mb-2 mt-6">
-            <span className="text-sm font-bold">Python</span>
-            <button
-              onClick={handleRun}
-              className="px-2 py-1 bg-blue-500 text-white text-xs rounded"
-            >
-              <Play size={18} />
-            </button>
-          </div>
-        )}
-        <SyntaxHighlighter PreTag="div" style={oneLight} language={language}>
-          {code}
-        </SyntaxHighlighter>
-        {language === 'python' && result?.output && (
-          <div className="mt-2 p-2 bg-gray-100 rounded">
-            <div className="mb-2">
-              <strong>Output</strong>
-            </div>
-            <div dangerouslySetInnerHTML={{ __html: result.output }} />
-          </div>
-        )}
-        {language === 'python' && base64Figures.length > 0 && (
-          <div className="mt-2 p-2 bg-gray-100 rounded">
-            <div>
-              <strong>Figures</strong>
-            </div>
-            <div className="grid grid-cols-1 gap-2">
-              {base64Figures.map((base64, i) => (
-                <div key={i} className="flex flex-col items-start mb-2">
-                  <a
-                    href={`data:image/png;base64,${base64}`}
-                    download={`figure-${i}-test.png`}
-                    className="my-2 self-end p-1 text-black underline flex items-center"
-                  >
-                    <Download size={20} />
-                  </a>
-                  <img
-                    src={`data:image/png;base64,${base64}`}
-                    alt={`figure-${i}`}
-                    className="max-w-full"
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
-        )}
-      </div>
-    )
-  }
+const ImageDisplay = ({ base64 }: { base64: string }) => (
+  <div className="flex flex-col items-start mb-2">
+    <a
+      href={`data:image/png;base64,${base64}`}
+      download={`figure.png`}
+      className="my-2 self-end p-1 text-black underline flex items-center"
+    >
+      <Download size={20} />
+    </a>
+    <img src={`data:image/png;base64,${base64}`} alt="figure" className="max-w-full" />
+  </div>
 )
 
-CodeBlock.displayName = 'CodeBlock'
+const CodeBlock: React.FC<CodeBlockProps> = ({
+  conversation,
+  messageId,
+  code,
+  handleExecutionResult,
+  language,
+  inline,
+  index
+}) => {
+  const [base64Figures, setBase64Figures] = useState<string[]>([])
+
+  const executionResult = useMemo(() => {
+    return conversation?.messages.find(
+      (message) => message.id === messageId && message.executionResults
+    )?.executionResults?.[0]
+  }, [conversation, messageId])
+
+  useEffect(() => {
+    console.log('called')
+    if (executionResult?.figurePaths && executionResult.figurePaths.length > 0) {
+      Promise.all(executionResult.figurePaths.map(loadBase64Data)).then((base64Images) => {
+        const filteredImages = base64Images.filter((base64) => base64 !== null) as string[]
+        setBase64Figures(filteredImages)
+      })
+    } else {
+      setBase64Figures([])
+    }
+  }, [executionResult])
+
+  const handleRun = async () => {
+    try {
+      const figureDir = await getConversationImagesDir(conversation.id)
+      const result = await runPythonCode(figureDir, code)
+      await saveConversationWithPythonResult(conversation, messageId, result)
+
+      if (result.figurePaths) {
+        const base64Images = (await Promise.all(result.figurePaths.map(loadBase64Data))).filter(
+          (base64) => base64 !== null
+        ) as string[]
+        setBase64Figures(base64Images)
+      }
+      handleExecutionResult(messageId, result)
+    } catch (error) {
+      const result: ExecutionResult = {
+        code,
+        error: (error as Error).message
+      }
+      handleExecutionResult(messageId, result)
+    }
+  }
+
+  if (inline) {
+    return (
+      <code key={index} className="bg-gray-300 rounded px-1 py-0.5 text-sm">
+        {code}
+      </code>
+    )
+  }
+
+  return (
+    <div>
+      {language === 'python' && (
+        <div className="flex justify-between items-center mb-2 mt-6">
+          <span className="text-sm font-bold">Python</span>
+          <button onClick={handleRun} className="px-2 py-1 bg-blue-500 text-white text-xs rounded">
+            <Play size={18} />
+          </button>
+        </div>
+      )}
+      <SyntaxHighlighter PreTag="div" style={oneLight} language={language}>
+        {code}
+      </SyntaxHighlighter>
+      {language === 'python' && executionResult?.output && (
+        <div className="mt-2 p-2 bg-gray-100 rounded">
+          <div className="mb-2">
+            <strong>Output</strong>
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: executionResult.output }} />
+        </div>
+      )}
+      {language === 'python' && base64Figures.length > 0 && (
+        <div className="mt-2 p-2 bg-gray-100 rounded">
+          <div>
+            <strong>Figures</strong>
+          </div>
+          <div className="grid grid-cols-1 gap-2">
+            {base64Figures.map((base64, i) => (
+              <ImageDisplay key={i} base64={base64} />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default CodeBlock
